e5b5107549f613ac93bd15b6861c1c1d269dadf7
roy <kimathiwaweru@gmail.com>
2023-03-15 00:58:27 +0300

    added dynamic routing with javascript

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-function routes () {
-    let pageLink = document.getElementsByTagName("a");
-    pageLink.addEventListener("click", () => {
-        
-
-
-    })
+const links = document.querySelectorAll("a.nav-link");
+const pages = []; 
+for (const link of links  ){
+      pages.push(document.getElementById(link.target));
+    
+     }
+console.log(pages)
+let pageToLoad = () => {
 
 }
+function loadPage (pageToLoad){
+  for (const page of pages){
+    console.log(page);
+    if(page.id === pageToLoad){
+      page.style.display = "block";
+    } else {
+      page.style.display = "none";
+    }
+  }
+}
+function index () {
+  loadPage("home");
+  for (const link of links){
+    console.log(link);
+    link.addEventListener("click", (e) => {
+      console.log (link.target)
+      e.preventDefault();
+      let pageToLoad = link.target;
+      console.log(pageToLoad);
+      loadPage(pageToLoad);
+
+    })
+    
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", index);
+
+
+
+
+
